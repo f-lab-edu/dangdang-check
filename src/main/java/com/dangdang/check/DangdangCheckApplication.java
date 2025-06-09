04ce61213991198ed42aad6d8493982ad38dbaed
duyeong0020 <duyeong0020@gmail.com>
2025-06-09 22:07:05 +0900

    feat: initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/java/com/dangdang/check/DangdangCheckApplication.java
@@ -0,0 +1,13 @@
+package com.dangdang.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangdangCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangdangCheckApplication.class, args);
+	}
+
+}
