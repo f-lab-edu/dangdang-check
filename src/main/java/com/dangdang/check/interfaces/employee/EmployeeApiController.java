68691a7f0f7926d49680393788cb0cceb1f20ff5
duyeong0020 <duyeong0020@gmail.com>
2025-06-26 15:30:48 +0900

    feat: 프로필 업데이트 API 추가

--- src/main/java/com/dangdang/check/interfaces/employee/EmployeeApiController.java
+++ src/main/java/com/dangdang/check/interfaces/employee/EmployeeApiController.java
@@ -2,28 +2,38 @@ package com.dangdang.check.interfaces.employee;
 
 
 import com.dangdang.check.common.response.CommonResponse;
+import com.dangdang.check.domain.employee.request.UpdateProfile;
 import com.dangdang.check.domain.employee.response.EmployeeInfo;
 import com.dangdang.check.domain.employee.EmployeeService;
 import com.dangdang.check.domain.employee.request.RegisterEmployee;
 import com.dangdang.check.interfaces.employee.request.RegisterEmployeeRequest;
+import com.dangdang.check.interfaces.employee.request.UpdateProfileRequest;
 import com.dangdang.check.interfaces.employee.response.RegisterEmployeeResponse;
+import com.dangdang.check.interfaces.employee.response.UpdateProfileResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/employees")
 @RequiredArgsConstructor
 public class EmployeeApiController {
 
     private final EmployeeService employeeService;
 
-    @PostMapping("/api/employees")
+    @PostMapping
     public CommonResponse<RegisterEmployeeResponse> registerEmployee(@RequestBody @Valid RegisterEmployeeRequest request) {
         RegisterEmployee command = request.toCommand();
         EmployeeInfo employeeInfo = employeeService.registerEmployee(command);
         RegisterEmployeeResponse response = new RegisterEmployeeResponse(employeeInfo);
+        return CommonResponse.success(response);
+    }
+
+    @PatchMapping("/me/profile")
+    public CommonResponse<UpdateProfileResponse> updateProfile(@RequestBody @Valid UpdateProfileRequest request) {
+        UpdateProfile command = request.toCommand();
+        EmployeeInfo employeeInfo = employeeService.updateProfile(command);
+        UpdateProfileResponse response = new UpdateProfileResponse(employeeInfo);
         return CommonResponse.success(response);
     }
 }
