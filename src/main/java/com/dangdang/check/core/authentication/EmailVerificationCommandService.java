dc34e385f3c9e689d3d7edae3b04deb8b1fa0789
duyeong0020 <duyeong0020@gmail.com>
2025-06-30 19:46:04 +0900

    feat: 트랜잭션 어노테이션 추가

--- src/main/java/com/dangdang/check/core/authentication/EmailVerificationCommandService.java
+++ src/main/java/com/dangdang/check/core/authentication/EmailVerificationCommandService.java
@@ -3,6 +3,7 @@ package com.dangdang.check.core.authentication;
 import com.dangdang.check.domain.verification.EmailVerification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,6 +11,7 @@ public class EmailVerificationCommandService {
 
     private final EmailVerificationRepository emailVerificationRepository;
 
+    @Transactional
     public EmailVerification save(EmailVerification emailVerification) {
         return emailVerificationRepository.save(emailVerification);
     }
--- src/main/java/com/dangdang/check/core/employee/EmployeeCommandService.java
+++ src/main/java/com/dangdang/check/core/employee/EmployeeCommandService.java
@@ -3,6 +3,7 @@ package com.dangdang.check.core.employee;
 import com.dangdang.check.domain.employee.EmployeeEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,15 +11,18 @@ public class EmployeeCommandService {
 
     private final EmployeeJpaRepository employeeJpaRepository;
 
+    @Transactional
     public EmployeeEntity save(EmployeeEntity employee) {
         return employeeJpaRepository.save(employee);
     }
 
+    @Transactional
     public EmployeeEntity updateProfile(EmployeeEntity employee, String name, String nickname) {
         employee.modifyProfile(name, nickname);
         return employee;
     }
 
+    @Transactional
     public EmployeeEntity updatePassword(EmployeeEntity employee, String password) {
         employee.modifyPassword(password);
         return employee;
