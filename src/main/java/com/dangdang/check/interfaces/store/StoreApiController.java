1e163e2fbf0476fd3d86efbcc844ecddfe21717b
duyeong0020 <duyeong0020@gmail.com>
2025-07-16 01:14:52 +0900

    feat: 가게 등록 신청 API 추가

---
+++ src/main/java/com/dangdang/check/interfaces/store/StoreApiController.java
@@ -0,0 +1,26 @@
+package com.dangdang.check.interfaces.store;
+
+
+import com.dangdang.check.common.response.CommonResponse;
+import com.dangdang.check.domain.store.StoreService;
+import com.dangdang.check.domain.store.response.StoreInfo;
+import com.dangdang.check.interfaces.store.request.RegisterStoreRequest;
+import com.dangdang.check.interfaces.store.response.RegisterStoreResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StoreApiController {
+
+    private final StoreService storeService;
+
+    @PostMapping("/api/store-requests")
+    public CommonResponse<RegisterStoreResponse> registerStore(@RequestBody @Valid RegisterStoreRequest request) {
+        StoreInfo storeInfo = storeService.registerStore(request.toCommand());
+        return CommonResponse.success(new RegisterStoreResponse(storeInfo));
+    }
+}
