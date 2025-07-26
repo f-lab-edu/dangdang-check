104b95fee2ebd899f29ee4a4b34c45f28ef4f4d7
duyeong0020 <duyeong0020@gmail.com>
2025-07-26 23:37:13 +0900

    feat: Update access token TTL and enhance security configuration with JWT and login filters

--- src/main/java/com/dangdang/check/common/constant/AuthConstants.java
+++ src/main/java/com/dangdang/check/common/constant/AuthConstants.java
@@ -3,6 +3,6 @@ package com.dangdang.check.common.constant;
 public class AuthConstants {
     public static final String ACCESS_TOKEN_TYPE = "access";
     public static final String REFRESH_TOKEN_TYPE = "refresh";
-    public static final long ACCESS_TOKEN_TTL_MS = 600_000L;
+    public static final long ACCESS_TOKEN_TTL_MS = 60_000_000L;
     public static final long REFRESH_TOKEN_TTL_MS = 86_400_000L;
 }
