2d71f5b3b8852fbdfa50d090d4e9e82a3957b959
duyeong0020 <duyeong0020@gmail.com>
2025-07-26 19:05:13 +0900

    feat: Implement JWT filter and validation exception for access token handling

---
+++ src/main/java/com/dangdang/check/common/filter/JwtFilter.java
@@ -0,0 +1,51 @@
+package com.dangdang.check.common.filter;
+
+import com.dangdang.check.common.constant.AuthConstants;
+import com.dangdang.check.core.auth.JwtService;
+import com.dangdang.check.domain.employee.response.EmployeeDetails;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authToken = request.getHeader(AuthConstants.ACCESS_TOKEN_TYPE);
+        if (isInvalidBearerToken(authToken)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String accessToken = authToken.substring(7);
+
+        try {
+            EmployeeDetails employeeDetails = jwtService.getEmployeeDetails(accessToken);
+            SecurityContextHolder
+                    .getContext()
+                    .setAuthentication(new UsernamePasswordAuthenticationToken(employeeDetails, null, employeeDetails.getAuthorities()));
+        } catch (ExpiredJwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private boolean isInvalidBearerToken(String token) {
+        return !StringUtils.hasText(token) || !token.startsWith("Bearer ");
+    }
+}
--- src/main/java/com/dangdang/check/core/auth/JwtService.java
+++ src/main/java/com/dangdang/check/core/auth/JwtService.java
@@ -1,7 +1,11 @@
 package com.dangdang.check.core.auth;
 
 import com.dangdang.check.common.constant.AuthConstants;
+import com.dangdang.check.common.exception.JwtValidationException;
 import com.dangdang.check.common.util.JwtUtil;
+import com.dangdang.check.domain.employee.Role;
+import com.dangdang.check.domain.employee.response.EmployeeDetails;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +15,32 @@ public class JwtService {
 
     private final JwtUtil jwtUtil;
 
+    public EmployeeDetails getEmployeeDetails(String token) {
+        try {
+            jwtUtil.isExpired(token);
+        } catch (ExpiredJwtException e) {
+            throw new JwtValidationException("access token expired", e);
+        }
+
+        if (isInvalidAccessToken(token)) {
+            throw new JwtValidationException("invalid access token");
+        }
+
+        String loginId = jwtUtil.getLoginId(token);
+        String role = jwtUtil.getRole(token);
+        return new EmployeeDetails(loginId, null, Role.valueOf(role));
+    }
+
+
     public String createAccessToken(String loginId, String role) {
         return jwtUtil.createJwt(AuthConstants.ACCESS_TOKEN_TYPE, loginId, role, AuthConstants.ACCESS_TOKEN_TTL_MS);
     }
 
     public String createRefreshToken(String loginId, String role) {
         return jwtUtil.createJwt(AuthConstants.REFRESH_TOKEN_TYPE, loginId, role, AuthConstants.REFRESH_TOKEN_TTL_MS);
+    }
+
+    private boolean isInvalidAccessToken(String token) {
+        return AuthConstants.ACCESS_TOKEN_TYPE.equals(jwtUtil.getCategory(token));
     }
 }
